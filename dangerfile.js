8f9b0e96f72f0f06eee96a3e982e7ae37a714bb0
Yurii <ypytusylnuk@trans.eu>
2024-09-04 16:43:51 +0200

    gh actions lint fix 2

--- dangerfile.js
+++ dangerfile.js
@@ -3,7 +3,7 @@ import path from 'path';
 import { dangerReassure } from 'reassure';
 
 dangerReassure(
-  // eslint-disable-next-line no-undef
   {
+    // eslint-disable-next-line no-undef
   inputFilePath: path.join(__dirname, './.reassure/output.md'),
 });
